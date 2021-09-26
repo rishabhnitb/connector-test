3d497fc09bbffc3e5776290777453374f8f7046d
CT-21-12 <CT-21-12@DESKTOP-1OUETUP>
2021-09-26 15:13:50 +0530

    changes in controller

--- connector/src/main/java/demo/connector/ConnController.java
+++ connector/src/main/java/demo/connector/ConnController.java
@@ -1,6 +1,5 @@
 package demo.connector;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,7 +10,6 @@ public class ConnController {
 
 	
 	@GET
-	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String testConn() {
 		
